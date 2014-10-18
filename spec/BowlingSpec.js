5563ce226a18eb7c082dd7b9d66e250c57ef7168
Stephen Giles <s.giles@hotmail.co.uk>
2014-10-18 18:08:57 +0100

    Ball class added. It can knock down pins and the Turn class can calculate the score from this.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,7 +1,15 @@
-xdescribe('A Game', function() {
+describe('A Game', function() {
 
 	it('should start with 10 turns', function() {
+		game = new Game();
+		expect(game.turns.length).toEqual(10);
+	});
 
+	xit('should know how to score a Strike', function() {
+		turn.pinsHit(10);
+		secondTurn.pinsHit(3);
+		secondTurn.pinsHit(5);
+		expect(turn.score).toEqual(18)
 	});
 });
 
@@ -9,27 +17,32 @@ describe('A Turn', function() {
 
 	beforeEach(function() {
 		turn = new Turn();
+		secondTurn = new Turn(2);
 	});
 
 	it('should start with 10 pins', function() {
 		expect(turn.pins).toEqual(10);
 	});
 
-	it('should drop pins when they are hit', function () {
-		turn.pinsHit(3)
-		expect(turn.pins).toEqual(7);
-	});
-
 	it('should calculate the score for the turn', function() {
-		turn.pinsHit(2)
-		turn.pinsHit(6)
-		turn.turnScore()
-		expect(turn.score).toEqual(8)
+		turn.bowls[0].pinsHit(2);
+		turn.bowls[1].pinsHit(6);
+		expect(turn.score).toEqual(8);
 	});
 
 	it('should know what turn it is in the game', function() {
-		secondTurn = new Turn(2);
 		expect(secondTurn.turnInGame).toEqual(2);
 	});
 
+	it('should be made up of two bowls', function() {
+		expect(turn.bowls.length).toEqual(2);
+	});
+});
+
+describe('A Bowl', function() {
+	it('should drop pins when they are hit', function() {
+		turn = new Turn(1);
+		turn.bowls[0].pinsHit(3)
+		expect(turn.pins).toEqual(7)
+	});
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,13 +1,26 @@
+function Game() {
+	this.turns = []
+	for(var i=0; i<10; i++){
+		this.turns.push(new Turn(i+1));
+	}
+};
+
 function Turn(number) {
 	this.pins = 10;
 	this.score = 0;
 	this.turnInGame = number;
+	this.bowls = [new Bowl(this), new Bowl(this)];
 };
 
-Turn.prototype.pinsHit = function(number) {
-	this.pins -= number;
-};
 
-Turn.prototype.turnScore = function() {
+Turn.prototype._turnScore = function() {
 	this.score = 10 - this.pins;
+};
+
+
+function Bowl(turn) {};
+
+Bowl.prototype.pinsHit = function(number) {
+	turn.pins -= number;
+	return turn._turnScore();
 };
